2bc343ce6532aac3aeb995d4b7213d8cc86b95ba
NiChloride <cl980820@hotmail.com>
2021-10-22 20:42:18 +0800

    first commit#

---
+++ connection.py
@@ -0,0 +1,22 @@
+import sys
+import socket
+import json
+import copy
+
+
+# ~~~~~============== CONNECTION ==============~~~~~
+def connect():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((exchange_hostname, port))
+    return s.makefile("rw", 1)
+
+
+def write_to_exchange(exchange, obj):
+    json.dump(obj, exchange)
+    exchange.write("\n")
+
+
+def read_from_exchange(exchange):
+    return json.loads(exchange.readline())
+
+
